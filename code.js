9fc46d5c69261a198a111b0d16e957897d3ed218
fialhogi <fialhogiselle@gmail.com>
2021-03-25 22:56:59 -0300

    Update code for new responsive

--- code.js
+++ code.js
@@ -1,34 +1,32 @@
 <mjml> 
-<mj-head>
-  <mj-style>
-    @media (max-width:320px) {
-      .header-txt {padding: 0 50px !important;}
-      .header-txt div {font-size:35px !important;}
-      .left-col div {height:60px !important;}
-      .right-col div {height:60px !important;}
-    }
+  <mj-head>
+    <mj-style>
+      @media (max-width:320px) {
+        .header-txt {padding: 0 50px !important;}
+        .header-txt div {font-size:35px !important;}
+        .header-txt {height:60px !important;}
+      }
 
-    @media (max-width:250px) {
-      .header-txt {padding: 0 50px !important;}
-      .header-txt div {font-size:35px !important;}
-      .left-col div {height:60px !important;}
-      .right-col div {height:60px !important;}
-    }
-  </mj-style>   
-</mj-head>
+      @media (max-width:250px) {
+        .header-txt {padding: 0 50px !important;}
+        .header-txt div {font-size:35px !important;}
+        .header-txt {height:60px !important;}
+      }
+    </mj-style>   
+  </mj-head>
   <mj-body>
     <mj-section background-color="#100b28" padding="35px 0">
       <mj-column>
           <mj-text css-class="header-txt" color="#fff" font-size="36px" padding="0 70px"font-weight="bold" font-family="saintecolombe-regular, Times New Roman, Times, serif">test <span style="font-family:Helvetica, Arial, sans-serif;">template</span></mj-text>
       </mj-column>
     </mj-section>
-    <mj-section background-color="#100b28" padding="25px 25px 0">
+    <mj-section background-color="#100b28" padding="0">
       <mj-group>
-        <mj-column>
-            <mj-image align="center" src="https://d2j29n432zojb.cloudfront.net/NTREIS-113523780-5.jpg" href="#" padding="0">
-            </mj-image>
-        </mj-group>
-      </mj-column>
+        <mj-column background-color="#100b28">
+          <mj-image align="center" src="https://d2j29n432zojb.cloudfront.net/NTREIS-113523780-5.jpg" href="#" padding="0 25px 0">
+          </mj-image>
+        </mj-column>
+      </mj-group>
     </mj-section> 
   </mj-body>
 </mjml>
